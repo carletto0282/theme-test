73471deba38a8ee9852324dfb30694d15ba04348
carletto0282 <info@studio4nea.it>
2019-10-10 15:32:16 +0200

    Enqueue scripts and styles

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php
 
-	add_action( 'wp_enqueue_scripts', 'tnt_enqueue_parent_styles' );
+    add_action( 'wp_enqueue_scripts', 'enqueue_custom_things' );
 
-	function tnt_enqueue_parent_styles() {
-	   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-	}
+    function enqueue_custom_things() {
+        // Enqueue bootstrap styles
+        wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css' );
+
+        // Enqueue parent styles
+        wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+        // Enqueue bootstrap JS
+        wp_enqueue_script( 'bootstrap-script', get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.min.js', array( 'jquery' ),null ,true );
+
+        // Enqueue Custom JS
+        wp_enqueue_script( 'theme-script', get_stylesheet_directory_uri().'/js/theme.js', array( 'jquery' ),null ,true );
+    }
